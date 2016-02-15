14ce607260bd7c0ea3ceb7b2b25f54d099c20eba
Eric Levasseur <eric.levass@gmail.com>
2016-02-15 09:14:18 -0500

    added scaffold and bootstrap gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-   get 'welcome/index'
+  root 'blog_post#index'
+  resources :comments
+  resources :blog_posts
+   #get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
+   #root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
